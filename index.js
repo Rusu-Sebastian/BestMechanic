4a7b6cb0fc24c9d9c9e069fd6fa732c050a2793f
Rusu-Sebastian <sebastianrusu01@gmail.com>
2024-04-12 12:22:46 +0300

    pimu comit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+//constante pentru crearea serverului
+const app = express();
+const port = 3000;
+
+//constante pentru a putea folosi path-ul
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//obiect informatii despre client
+class client{
+    nume;
+    prenume;
+    nrTelefon;
+    email;
+    nrMasini;
+}
+
+//obiect cu informatiile despre masina clientului
+class masina{
+    nrDeInmatriculare;
+    serieSasiu;
+    marca;
+    model;
+    anFabricatie;
+    tipMotorizare;
+    capacitateMotor;
+    caiPutere;
+    kwPutere=caiPutere/1.36;
+    cutieDeViteza;
+}
+
+//trimite fisierul index.html la client
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//middleware pentru a putea folosi body-parser
+app.use(express.urlencoded({ extended: true }));
+
+//primirea formularului de la client si stocarea informatiilor in obiectul client
+app.post('/submit', (req, res) => {
+    const clientNou = new client();
+    clientNou.nume = req.body.nume;
+    clientNou.prenume = req.body.prenume;
+    clientNou.nrTelefon = req.body.nrTelefon;
+    clientNou.email = req.body.email;
+    clientNou.nrMasini = req.body.nrMasini;
+    console.log(clientNou);
+    console.log(req.body);
+});
+
+//primirea formularului de la client si stocarea informatiilor in obiectul masina
+app.post('/submitCar', (req, res) => {
+    console.log(req.body);
+});
+
+//pornirea serverului
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+  
+
+//middleware pentru a trata cazurile in care ruta nu exista
+app.use((req, res) => {
+    res.status(404).send('Not Found');
+});
